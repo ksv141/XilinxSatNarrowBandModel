16aa3a911cee5096799982afb7e3a67ebbc67b1a
ksv141 <ksv_141@mail.ru>
2021-08-26 13:13:05 +0300

    qam4 desision

--- constellation.cpp
+++ constellation.cpp
@@ -12,3 +12,25 @@ xip_complex constell_preamble[2] = { {4096, 4096},
 
 // нормированная мощность сигнала qam4 (используется для АРУ демодулятора)
 xip_real pwr_constell_qam4 = 2 * constell_qam4[0].re * constell_qam4[0].re;
+
+xip_complex nearest_point_qam4(const xip_complex& in)
+{
+	return constell_qam4[nearest_index_qam4(in)];
+}
+
+int nearest_index_qam4(const xip_complex& in)
+{
+	if (in.re < 0) {
+		if (in.im < 0)
+			return 3;
+		else
+			return 2;
+	}
+	else {
+		if (in.im < 0)
+			return 1;
+		else
+			return 0;
+	}
+	return 0;
+}
--- constellation.h
+++ constellation.h
@@ -10,4 +10,8 @@ extern xip_complex constell_preamble[2];
 
 extern xip_real pwr_constell_qam4;
 
+extern xip_complex nearest_point_qam4(const xip_complex& in);
+
+extern int nearest_index_qam4(const xip_complex& in);
+
 #endif // CONSTELLATION_H
